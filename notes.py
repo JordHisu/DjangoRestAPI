b95722015852c79d667159ce7c2da0baa4ea6dd4
JordVCS <jord_aqui@hotmail.com>
2023-06-13 18:53:44 -0300

    added notes.py

---
+++ notes.py
@@ -0,0 +1,8 @@
+
+
+# https://github.com/CSM41-WEB/atividade-02-JordHisu/tree/master
+# https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
+# https://awsacademy.instructure.com/courses/41555/modules
+
+
+
